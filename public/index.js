79f77fe3a3d5d786ea439b1176020247e4fcb33c
NamBui98 <bvnam98@gmail.com>
2022-12-06 02:35:14 +0700

    update

--- public/index.js
+++ public/index.js
@@ -50,13 +50,13 @@ socket.on('welcome', async function (currentUser, currentUsers) {
 
 
     debugger
-    ctx.globalCompositeOperation = "source-over";
-    // Lets reduce the opacity of the BG paint to give the final touch
-    ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-    ctx.fillRect(0, 0, W, H);
+    // ctx.globalCompositeOperation = "source-over";
+    // // Lets reduce the opacity of the BG paint to give the final touch
+    // ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+    // ctx.fillRect(0, 0, W, H);
 
-    //Lets blend the particle with the BG
-    ctx.globalCompositeOperation = "lighter";
+    // //Lets blend the particle with the BG
+    // ctx.globalCompositeOperation = "lighter";
 
     //players in lobby
     for (var i = 0; i < currentUsers.length; i++) {
@@ -76,102 +76,102 @@ socket.on('welcome', async function (currentUser, currentUsers) {
     }
 
     //player
-    ctx.beginPath();
-    //Time for some colors
-    var gradient = ctx.createRadialGradient(currentUser.x, currentUser.y, 0, currentUser.x, currentUser.y, currentUser.radius);
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(0.4, "white");
-    gradient.addColorStop(0.4, currentUser.color);
-    gradient.addColorStop(1, "black");
+    // ctx.beginPath();
+    // //Time for some colors
+    // var gradient = ctx.createRadialGradient(currentUser.x, currentUser.y, 0, currentUser.x, currentUser.y, currentUser.radius);
+    // gradient.addColorStop(0, "white");
+    // gradient.addColorStop(0.4, "white");
+    // gradient.addColorStop(0.4, currentUser.color);
+    // gradient.addColorStop(1, "black");
 
-    ctx.fillStyle = gradient;
-    ctx.arc(currentUser.x, currentUser.y, currentUser.radius, Math.PI * 2, false);
-    ctx.fill();
+    // ctx.fillStyle = gradient;
+    // ctx.arc(currentUser.x, currentUser.y, currentUser.radius, Math.PI * 2, false);
+    // ctx.fill();
 });
 
 //other users get updated with new players when teh new player joins
 socket.on('currentUsers', function (currentUsers) {
-    ctx.globalCompositeOperation = "source-over";
-    //Lets reduce the opacity of the BG paint to give the final touch
-    ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+    // ctx.globalCompositeOperation = "source-over";
+    // //Lets reduce the opacity of the BG paint to give the final touch
+    // ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
 
-    //Lets blend the particle with the BG
-    ctx.globalCompositeOperation = "lighter";
+    // //Lets blend the particle with the BG
+    // ctx.globalCompositeOperation = "lighter";
 
     for (var i = 0; i < currentUsers.length; i++) {
 
-        ctx.beginPath();
+        // ctx.beginPath();
 
-        //Time for some colors
-        var gradient = ctx.createRadialGradient(currentUsers[i].x, currentUsers[i].y, 0, currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius);
-        gradient.addColorStop(0, "white");
-        gradient.addColorStop(0.4, "white");
-        gradient.addColorStop(0.4, currentUsers[i].color);
-        gradient.addColorStop(1, "black");
+        // //Time for some colors
+        // var gradient = ctx.createRadialGradient(currentUsers[i].x, currentUsers[i].y, 0, currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius);
+        // gradient.addColorStop(0, "white");
+        // gradient.addColorStop(0.4, "white");
+        // gradient.addColorStop(0.4, currentUsers[i].color);
+        // gradient.addColorStop(1, "black");
 
-        ctx.fillStyle = gradient;
-        ctx.arc(currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius, Math.PI * 2, false);
-        ctx.fill();
+        // ctx.fillStyle = gradient;
+        // ctx.arc(currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius, Math.PI * 2, false);
+        // ctx.fill();
     }
     console.log('A new User has joined');
 });
 
 //if a player leaves, everyone gets new set of players
 socket.on('playerLeft', function (currentUsers) {
-    ctx.fillRect(0, 0, W, H);
-    ctx.globalCompositeOperation = "source-over";
-    //Lets reduce the opacity of the BG paint to give the final touch
-    ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+    // ctx.fillRect(0, 0, W, H);
+    // ctx.globalCompositeOperation = "source-over";
+    // //Lets reduce the opacity of the BG paint to give the final touch
+    // ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
 
-    //Lets blend the particle with the BG
-    ctx.globalCompositeOperation = "lighter";
+    // //Lets blend the particle with the BG
+    // ctx.globalCompositeOperation = "lighter";
 
     for (var i = 0; i < currentUsers.length; i++) {
 
-        ctx.beginPath();
+        // ctx.beginPath();
 
-        //Time for some colors
-        var gradient = ctx.createRadialGradient(currentUsers[i].x, currentUsers[i].y, 0, currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius);
-        gradient.addColorStop(0, "white");
-        gradient.addColorStop(0.4, "white");
-        gradient.addColorStop(0.4, currentUsers[i].color);
-        gradient.addColorStop(1, "black");
+        // //Time for some colors
+        // var gradient = ctx.createRadialGradient(currentUsers[i].x, currentUsers[i].y, 0, currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius);
+        // gradient.addColorStop(0, "white");
+        // gradient.addColorStop(0.4, "white");
+        // gradient.addColorStop(0.4, currentUsers[i].color);
+        // gradient.addColorStop(1, "black");
 
-        ctx.fillStyle = gradient;
-        ctx.arc(currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius, Math.PI * 2, false);
-        ctx.fill();
+        // ctx.fillStyle = gradient;
+        // ctx.arc(currentUsers[i].x, currentUsers[i].y, currentUsers[i].radius, Math.PI * 2, false);
+        // ctx.fill();
     }
     console.log('A Player Has left');
 });
 
 
 socket.on('PlayersMoving', function (players) {
-    ctx.globalCompositeOperation = "source-over";
-    //Lets reduce the opacity of the BG paint to give the final touch
-    ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-    ctx.fillRect(0, 0, W, H);
+    // ctx.globalCompositeOperation = "source-over";
+    // //Lets reduce the opacity of the BG paint to give the final touch
+    // ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+    // ctx.fillRect(0, 0, W, H);
 
-    //Lets blend the particle with the BG
-    ctx.globalCompositeOperation = "lighter";
+    // //Lets blend the particle with the BG
+    // ctx.globalCompositeOperation = "lighter";
 
-    var players = players;
-    var i = 0;
-    function allPlayers() {
-        for (i; i < players.length; i++) {
+    // var players = players;
+    // var i = 0;
+    // function allPlayers() {
+    //     for (i; i < players.length; i++) {
 
-            ctx.beginPath();
+    //         ctx.beginPath();
 
-            //Time for some colors
-            var gradient = ctx.createRadialGradient(players[i].x, players[i].y, 0, players[i].x, players[i].y, players[i].radius);
-            gradient.addColorStop(0.5, "white");
-            gradient.addColorStop(0.5, players[i].color);
-            gradient.addColorStop(1, "black");
+    //         //Time for some colors
+    //         var gradient = ctx.createRadialGradient(players[i].x, players[i].y, 0, players[i].x, players[i].y, players[i].radius);
+    //         gradient.addColorStop(0.5, "white");
+    //         gradient.addColorStop(0.5, players[i].color);
+    //         gradient.addColorStop(1, "black");
 
-            ctx.fillStyle = gradient;
-            ctx.arc(players[i].x, players[i].y, players[i].radius, Math.PI * 2, false);
-            ctx.fill();
-        }
-    }
-    allPlayers();
+    //         ctx.fillStyle = gradient;
+    //         ctx.arc(players[i].x, players[i].y, players[i].radius, Math.PI * 2, false);
+    //         ctx.fill();
+    //     }
+    // }
+    // allPlayers();
 
 });
